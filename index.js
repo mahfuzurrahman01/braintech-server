62186797759b893d03f3f4ea78e1a773be3bf176
mahfuzurrahman01 <mahfuzurrahman01010101@gmail.com>
2022-10-26 00:24:39 +0600

    courses json file data updated

--- index.js
+++ index.js
@@ -1,9 +1,15 @@
 const express = require('express')
+const cors = require ('cors')
 const app = express()
 const port = process.env.port || 5000;
-app.get('/',(req,res)=>{
+const courses = require('./data/Courses.json')
+app.get('/', (req, res) => {
     res.send('hey there server is running')
+})
+app.use(cors())
+app.get('/courses', (req, res) => {
+    res.send(courses)
 })
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
-  })
+})
