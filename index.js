afc94a921b0125718aef09e7b1104e01ee52ae11
mahfuzurrahman01 <mahfuzurrahman01010101@gmail.com>
2022-10-26 10:45:11 +0600

    request path data added in index.js file

--- index.js
+++ index.js
@@ -1,14 +1,20 @@
-const express = require('express')
-const cors = require ('cors')
-const app = express()
+const express = require('express');
+const cors = require ('cors');
+const app = express();
 const port = process.env.port || 5000;
-const courses = require('./data/Courses.json')
+const courses = require('./data/Courses.json');
+const details = require ('./data/Details.json');
 app.get('/', (req, res) => {
     res.send('hey there server is running')
 })
 app.use(cors())
 app.get('/courses', (req, res) => {
     res.send(courses)
+})
+app.get('/courses/:id',(req,res) =>{
+    const id = req.params.id;
+    const item = details.find(x => (x.id === id))
+    res.send(item)
 })
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
